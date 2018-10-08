73899cd128ddf0b74e2d0ae5aec570322879c44b
ManjulaHH <40054557+ManjulaHH@users.noreply.github.com>
2018-10-07 22:18:00 -1200

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screens/home/Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+ReactDOM.render(<Home/>, document.getElementById('root'));
 serviceWorker.unregister();
