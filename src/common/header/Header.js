a5253bba38a07414571aad6c6335ffe1050f1676
ManjulaHH <40054557+ManjulaHH@users.noreply.github.com>
2018-10-09 22:28:27 -1200

    Fixed issues related to state in modal for login & register

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -45,7 +45,12 @@ class Header extends Component{
     }
 
     openModalHandler = () => {
-        this.setState({modalIsOpen:true})
+        this.setState({
+            modalIsOpen: true,
+            value: 0,
+            usernameRequired: "dispNone",
+            username: ""
+        });
     }
 
     closeModalHandler = () => {
